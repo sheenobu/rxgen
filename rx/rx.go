0dd88d40ddd7528010d81a19abcd3974488e5a9d
Sheena Artrip <sheena.artrip@gmail.com>
2016-03-22 04:03:24 -0400

    Fixed stuttering of names

--- rx/rx.go
+++ rx/rx.go
@@ -1,20 +1,20 @@
-// package rx contains core types wrapped as reactive structures
+// Package rx contains core types wrapped as reactive structures
 package rx
 
 // generate some core types
 
-//go:generate rxgen -name RxUint8 -type uint8 .
-//go:generate rxgen -name RxInt8 -type int8 .
+//go:generate rxgen -name Uint8 -type uint8 .
+//go:generate rxgen -name Int8 -type int8 .
 
-//go:generate rxgen -name RxUint16 -type uint16 .
-//go:generate rxgen -name RxInt16 -type int16 .
+//go:generate rxgen -name Uint16 -type uint16 .
+//go:generate rxgen -name Int16 -type int16 .
 
-//go:generate rxgen -name RxUint32 -type uint32 .
-//go:generate rxgen -name RxInt32 -type int32 .
+//go:generate rxgen -name Uint32 -type uint32 .
+//go:generate rxgen -name Int32 -type int32 .
 
-//go:generate rxgen -name RxUint64 -type uint64 .
-//go:generate rxgen -name RxInt64 -type int64 .
+//go:generate rxgen -name Uint64 -type uint64 .
+//go:generate rxgen -name Int64 -type int64 .
 
-//go:generate rxgen -name RxBool -type bool .
+//go:generate rxgen -name Bool -type bool .
 
-//go:generate rxgen -name RxString -type string .
+//go:generate rxgen -name String -type string .
